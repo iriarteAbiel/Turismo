811d2c846014ce0d517a7d4246f39503d3c1bcc3
iriarteAbiel <141873131+iriarteAbiel@users.noreply.github.com>
2024-09-23 03:08:08 -0400

    Proyecto Configurado

---
+++ TiquiTour/app/(tabs)/Styles/exploreStyles.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { Colors } from './../../../constants/Colors'
+
+const ExploreStyles = StyleSheet.create({
+    textSize:{
+        fontSize:100,
+        
+    }
+
+});
+export default ExploreStyles;
---
+++ TiquiTour/app/(tabs)/explore.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ExploreStyles from './../../app/(tabs)/Styles/exploreStyles'
+
+export default function explore() {
+  return (
+    <View>
+      <Text style={ExploreStyles.textSize} >explore</Text>
+    </View>
+  )
+}
